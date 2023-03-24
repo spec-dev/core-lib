f2be4e5e1217df218a6be664f10b02cc0ce93102
Ben Whittle <benwhittle31@gmail.com>
2023-03-24 15:05:15 -0500

    BigNumber -> BigInt

--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -1,5 +1,9 @@
 export * from './constants'
 export * from './formatters'
 export { saveAll } from './db'
-export { BigNumber, BigNumberish, isBigNumberish } from './ethers/bignumber'
+export {
+    BigNumber as BigInt,
+    BigNumberish as BigIntish,
+    isBigNumberish as isBigIntish,
+} from './ethers/bignumber'
 export { FixedNumber, FixedFormat, parseFixed, formatFixed } from './ethers/fixednumber'
