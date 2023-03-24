cd2a70d8e25aa09dfbcb601272b9e7e4046f32c1
Ben Whittle <benwhittle31@gmail.com>
2023-03-24 14:29:43 -0500

    v0.0.42 - ethers numbers

--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -1,3 +1,5 @@
 export * from './constants'
 export * from './formatters'
 export { saveAll } from './db'
+export { BigNumber, BigNumberish, isBigNumberish } from './ethers/bignumber'
+export { FixedNumber, FixedFormat, parseFixed, formatFixed } from './ethers/fixednumber'
