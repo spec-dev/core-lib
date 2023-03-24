999a0af2d90bb1bc5f7c76cd5769eebce35bd6b7
Ben Whittle <benwhittle31@gmail.com>
2023-03-23 22:17:06 -0500

    v0.0.37

--- src/lib/utils/caller.ts
+++ src/lib/utils/caller.ts
@@ -1,33 +1,34 @@
 export interface Bind {
-    cb?: (file: string) => string;
+    cb?: (file: string) => string
 }
 
-export const up = 3;
+export const up = 3
 
 export default function caller(this: Bind | any, levelUp = up) {
-    const err = new Error();
-    const stack = err.stack?.split('\n')[levelUp];
+    const err = new Error()
+    const stack = err.stack?.split('\n')[levelUp]
     if (stack) {
-        return getFile.bind(this)(stack);
+        return getFile.bind(this)(stack)
     }
 }
 
 export function getFile(this: Bind | any, stack: string) {
-    stack = stack.substring(stack.indexOf('at ') + 3);
+    stack = stack.substring(stack.indexOf('at ') + 3)
     if (!stack.startsWith('file://')) {
-        stack = stack.substring(stack.lastIndexOf('(') + 1);
+        stack = stack.substring(stack.lastIndexOf('(') + 1)
     }
-    const path = stack.split(':');
-    let file;
-    if (Deno.build.os == "windows") {
-        file = `${path[0]}:${path[1]}:${path[2]}`;
+    const path = stack.split(':')
+    let file
+    //@ts-ignore
+    if (Deno.build.os == 'windows') {
+        file = `${path[0]}:${path[1]}:${path[2]}`
     } else {
-        file = `${path[0]}:${path[1]}`;
+        file = `${path[0]}:${path[1]}`
     }
 
     if ((this as Bind)?.cb) {
-        const cb = (this as Bind).cb as any;
-        file = cb(file);
+        const cb = (this as Bind).cb as any
+        file = cb(file)
     }
-    return file;
+    return file
 }
