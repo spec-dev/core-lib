5de4194b0a3b55f8213e50366bb8077266d80c08
Ben Whittle <benwhittle31@gmail.com>
2023-06-15 14:01:40 -0500

    add contract registration queue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './lib/decorators'
 import LiveObject from './lib/liveObject'
-import PublishEventQueue from './lib/publishEventQueue'
-export { LiveObject, PublishEventQueue }
+import Queue from './lib/queue'
+export { LiveObject, Queue }
 export * from './lib/types'
 export * from './lib/helpers'
--- src/lib/liveObject.ts
+++ src/lib/liveObject.ts
@@ -22,7 +22,7 @@ import {
 } from './types'
 import { schemaForChainId } from '@spec.types/spec'
 import { toNamespacedVersion, fromNamespacedVersion, toArrayOfArrays } from './utils/formatters'
-import PublishEventQueue from './publishEventQueue'
+import Queue from './queue'
 import Properties from './properties'
 import { upsert, select } from './tables'
 import humps from './utils/humps'
@@ -74,7 +74,9 @@ class LiveObject {
 
     declare chainId: ChainId
 
-    _publishEventQueue: PublishEventQueue
+    _publishEventQueue: Queue
+
+    _contractRegistrationQueue: Queue
 
     _properties: Properties
 
@@ -84,8 +86,13 @@ class LiveObject {
         return this._publishEventQueue.items()
     }
 
-    constructor(publishEventQueue: PublishEventQueue) {
+    get _newContractInstances(): StringKeyMap[] {
+        return this._contractRegistrationQueue.items()
+    }
+
+    constructor(publishEventQueue: Queue, contractRegistrationQueue: Queue) {
         this._publishEventQueue = publishEventQueue
+        this._contractRegistrationQueue = contractRegistrationQueue
 
         let uniqueBy = Array.isArray(this._options.uniqueBy)
             ? this._options.uniqueBy
@@ -178,7 +185,7 @@ class LiveObject {
 
     new(liveObjectType, initialProperties: StringKeyMap = {}) {
         // Create new live object and pass event response queue for execution context.
-        const newLiveObject = new liveObjectType(this._publishEventQueue)
+        const newLiveObject = new liveObjectType(this._publishEventQueue, this._contractRegistrationQueue)
         newLiveObject._tablesApiToken = this._tablesApiToken
 
         // Pass any block-specific properties over unless included in properties given.
@@ -235,7 +242,7 @@ class LiveObject {
         // Convert back into live object type / property types.
         return await Promise.all(
             records.map(async (record) => {
-                const liveObject = new liveObjectType(this._publishEventQueue)
+                const liveObject = new liveObjectType(this._publishEventQueue, this._contractRegistrationQueue)
                 await liveObject._fsPromises()
                 liveObject._tablesApiToken = this._tablesApiToken
                 const propertyData = liveObject._properties.fromRecord(record)
@@ -352,6 +359,10 @@ class LiveObject {
 
     publishEvent(name: string, data: StringKeyMap) {
         this._publishEventQueue.push({ name, data })
+    }
+    
+    registerContract(address: string, chainId: string, group: string) {
+        this._contractRegistrationQueue.push({ address, chainId, group })
     }
 
     async getEventName(): Promise<string> {
